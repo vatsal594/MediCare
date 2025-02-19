2ac9e8d39939196bbbae38fae89df83fcb5a6fa1
vatsal <vatsalsavani594@gmail.com>
2025-02-19 13:40:26 +0530

    welcome message navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -46,26 +46,29 @@ const Navbar = () => {
 
       <div className="flex items-center gap-4">
         {token && userData ? (
-          <div className="flex items-center gap-2 cursor-pointer group relative">
-            <img className="w-8 rounded-full" src={userData.image} alt="" />
-            <img className="w-2.5" src={assets.dropdown_icon} alt="" />
-            <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
-              <div className="min-w-48 bg-gray-50 rounded flex flex-col gap-4 p-4">
-                <p
-                  onClick={() => navigate("/my-profile")}
-                  className="hover:text-black cursor-pointer"
-                >
-                  My Profile
-                </p>
-                <p
-                  onClick={() => navigate("/my-appointments")}
-                  className="hover:text-black cursor-pointer"
-                >
-                  My Appointments
-                </p>
-                <p onClick={logout} className="hover:text-black cursor-pointer">
-                  Logout
-                </p>
+          <div className="flex items-center gap-4">
+            <p className="text-gray-700 font-medium">Welcome, {userData.name}!</p>
+            <div className="flex items-center gap-2 cursor-pointer group relative">
+              <img className="w-8 rounded-full" src={userData.image} alt="" />
+              <img className="w-2.5" src={assets.dropdown_icon} alt="" />
+              <div className="absolute top-0 right-0 pt-14 text-base font-medium text-gray-600 z-20 hidden group-hover:block">
+                <div className="min-w-48 bg-gray-50 rounded flex flex-col gap-4 p-4">
+                  <p
+                    onClick={() => navigate("/my-profile")}
+                    className="hover:text-black cursor-pointer"
+                  >
+                    My Profile
+                  </p>
+                  <p
+                    onClick={() => navigate("/my-appointments")}
+                    className="hover:text-black cursor-pointer"
+                  >
+                    My Appointments
+                  </p>
+                  <p onClick={logout} className="hover:text-black cursor-pointer">
+                    Logout
+                  </p>
+                </div>
               </div>
             </div>
           </div>
