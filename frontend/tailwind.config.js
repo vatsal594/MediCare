84c516dd36b9ef09fd118cd4361e815b3eaf656e
vatsal <vatsalsavani594@gmail.com>
2025-02-28 18:36:36 +0530

    changes in modules

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./index.html",          // Make sure your HTML files are included
+    "./src/**/*.{js,ts,jsx,tsx}", // This ensures all relevant JS/TS/JSX/TSX files are considered
   ],
   theme: {
     extend: {
-      gridTemplateColumns:{
-        'auto':'repeat(auto-fill, minmax(200px, 1fr))'
+      gridTemplateColumns: {
+        'auto': 'repeat(auto-fill, minmax(200px, 1fr))', // Responsive grid setup
       },
-      colors:{
-        'primary':'#5F6FFF'
-      }
+      colors: {
+        'primary': '#5F6FFF',  // Custom primary color
+      },
     },
   },
   plugins: [],
